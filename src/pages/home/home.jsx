e34a04022550a6291d5266dc75aabb7aa70fe342
Jhon Zambrano <jjzambrano@ucundinamarca.edu.co>
2023-10-11 22:48:57 -0500

    Se agregp botones de ver y subir documentos

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,6 @@
 import './home.css';
 import { useState, useEffect } from 'react';
-import logo from '../../assets/react.svg'
+import logo from '../../assets/img/U.png';
 
 function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -8,30 +8,33 @@ function Home() {
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
-    }, 5000); 
+    }, 5000);
   }, []);
 
   return (
     <div className='container'>
-        <header className='header'>
-            <img src={logo} alt="titulo" />
-            <img src={logo} alt="logoU" />
-            <img src={logo} alt="logoU" />
-            {/* desplegable o boton para iniciar sesion usuario  */}
-            </header>      
+      <header className='header'>
+        <img src={logo} alt="titulo" />
+        <img src={logo} alt="logoU" className='logoU' />        
+        {/* desplegable o boton para iniciar sesión de usuario */}
+        <button className='button-top-right' onClick={() => alert('Botón clickeado')}>INGRESAR</button>
+      </header>
       {isLoading ? (
-        
         <div className="loading-overlay loading">
-            <img src={logo} alt="" />
+          <img src={logo} alt="" />
           <h2>Universidad de Cundinamarca</h2>
         </div>
       ) : (
         <div className='container-main'>
           <div className="main">
-            <h1>Hola</h1>
+            <h1>GUIAS Y MANUALES DE USUARIO</h1>
+            <p>Bienvenido a nuestra plataforma de guías y manuales de usuario.</p>
+            <div className="buttons-container">
+              <button className="main-button">Ver </button>
+              <button className="main-button">Subir </button>
+            </div>
           </div>
         </div>
-        
       )}
     </div>
   );
