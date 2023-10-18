25531f4570969a397da8e530469e0f22f1b09740
Santiago Rodriguez <santi-1306@outlook.com>
2023-10-18 12:17:07 -0500

    Se corrigio rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-// En tu archivo de rutas principal (App.js o donde definas las rutas)
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/home/home';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/home";
 // import VerDocumentos from './pages/'; // Componente para mostrar documentos
 // import SubirDocumento from './SubirDocumento'; // Componente para cargar documentos
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/ver-documentos" component={VerDocumentos} />
-        <Route path="/subir-documento" component={SubirDocumento} />
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Home />} />
+      </Routes>
+      {/* <Route path="/ver-documentos" component={VerDocumentos} />
+        <Route path="/subir-documento" component={SubirDocumento} /> */}
+    </BrowserRouter>
   );
 }
 
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,7 @@
-import './home.css';
-import { useState, useEffect } from 'react';
-import logo from '../../assets/img/U.png';
+/* eslint-disable no-unused-vars */
+import "./home.css";
+import { useState, useEffect } from "react";
+import logo from "../../assets/img/U.png";
 
 function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -13,24 +14,25 @@ function Home() {
   }, []);
 
   const handleViewFile = () => {
-    // Lógica para ver un archivo específico
-    // Por ejemplo, abrir un visor de PDF u otro tipo de archivo
-    alert('Ver archivo específico');
+    alert("Ver archivo específico");
   };
 
   const handleUploadFiles = (e) => {
     const selectedFiles = Array.from(e.target.files);
-    // Puedes realizar acciones con los archivos seleccionados, como enviarlos al servidor
-    // Aquí puedes procesar o almacenar los archivos seleccionados en el estado
     setFilesToUpload(selectedFiles);
   };
 
   return (
-    <div className='container'>
-      <header className='header'>
+    <div className="container">
+      <header className="header">
         <img src={logo} alt="titulo" />
-        <img src={logo} alt="logoU" className='logoU' />        
-        <button className='button-top-right' onClick={() => alert('Botón clickeado')}>INGRESAR</button>
+        <img src={logo} alt="logoU" className="logoU" />
+        <button
+          className="button-top-right"
+          onClick={() => alert("Botón clickeado")}
+        >
+          INGRESAR
+        </button>
       </header>
       {isLoading ? (
         <div className="loading-overlay loading">
@@ -38,12 +40,16 @@ function Home() {
           <h2>Universidad de Cundinamarca</h2>
         </div>
       ) : (
-        <div className='container-main'>
+        <div className="container-main">
           <div className="main">
             <h1>GUIAS Y MANUALES DE USUARIO</h1>
-            <p>Bienvenido a nuestra plataforma de guías y manuales de usuario.</p>
+            <p>
+              Bienvenido a nuestra plataforma de guías y manuales de usuario.
+            </p>
             <div className="buttons-container">
-              <button className="main-button" onClick={handleViewFile}>Ver</button>
+              <button className="main-button" onClick={handleViewFile}>
+                Ver
+              </button>
               <input
                 type="file"
                 className="main-button button-upload"
