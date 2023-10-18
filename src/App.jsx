6b2fa893e48b1a8661892ed7f694b7790e030e73
Jhon Zambrano <jjzambrano@ucundinamarca.edu.co>
2023-10-18 12:04:00 -0500

    cambio ruta home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // En tu archivo de rutas principal (App.js o donde definas las rutas)
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import VerDocumentos from './VerDocumentos'; // Componente para mostrar documentos
-import SubirDocumento from './SubirDocumento'; // Componente para cargar documentos
+import Home from './pages/home/home';
+// import VerDocumentos from './pages/'; // Componente para mostrar documentos
+// import SubirDocumento from './SubirDocumento'; // Componente para cargar documentos
 
 function App() {
   return (
